3d925e7213e874c9c268160910547d6e777bc5ee
danidz96 <37420305+danidz96@users.noreply.github.com>
2018-07-21 00:23:39 +0200

    show search results

--- src/js/index.js
+++ src/js/index.js
@@ -17,24 +17,21 @@ const state = {};
 const controlSearch = async () => {
     // 1) Get query from view
     const query = searchView.getInput(); 
-    
+
     if (query) {
         // 2) New search object and add to state
         state.search = new Search(query);
 
         // 3) Prepare UI for results
-        
-
-        try {
-            // 4) Search for recipes
-            await state.search.getResults();
+        searchView.clearInput();
+        searchView.clearResults();
     
-            // 5) Render results on UI
-            console.log(state.search.result);
-            
-        } catch (err) {
-            alert('Something wrong with the search...');
-        }
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        searchView.renderResults(state.search.result);
+
     }
 }
 
@@ -42,7 +39,3 @@ elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
-
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
