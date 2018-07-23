91bbffcbb65c09b2b6928a09a7de2961e523510c
danidz96 <37420305+danidz96@users.noreply.github.com>
2018-07-23 21:57:29 +0200

    implement localStorage

--- src/js/index.js
+++ src/js/index.js
@@ -140,10 +140,6 @@ elements.shopping.addEventListener('click', e => {
  * LIKES CONTROLLER
 */
 
-//TESTING
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
-
 const controlLike = () => {
     if (!state.likes) state.likes = new Likes();
 
@@ -164,15 +160,30 @@ const controlLike = () => {
     } else {
         // Remove like from the state
         state.likes.deleteLike(currentID);
+
         // Toggle the like button
         likesView.toggleLikeBtn(false);
 
         //Remove like from UI list
-        likesView.likes.deleteLike(currentID);
+        likesView.deleteLike(currentID);
     }
 
     likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
+
+// Restore like recipes on page load
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+
+    // Restore likes
+    state.likes.readStorage();
+
+    // Toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+    // Render the existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
